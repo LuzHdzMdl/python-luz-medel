cbf390e8fbc75c7cfa570cd37a6be6272b1d5176
LuzHdzMdl <zs18021309@estudiantes.uv.mx>
2021-03-26 16:25:27 +0000

    variables

---
+++ variables.py
@@ -0,0 +1,18 @@
+#Variables.py
+#por luz medel
+#marzo 2021
+#
+year = 2021
+sentence = "Hello, my name is Luz"
+sarah, bob, mike = 16, 20, 25
+name, age = "luz", 21
+mari = fer = 20
+PI= 3.1416
+e = 2.71
+
+#display values
+
+print "year=", year
+print sentence
+print "\n sarah : ", sarah, "\n bob =", bob, "\n mike =", mike
+
