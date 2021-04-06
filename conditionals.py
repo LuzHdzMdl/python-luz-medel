8dc85030b9a2225431f857b4d5a8dfbd36f861f8
LuzHdzMdl <zs18021309@estudiantes.uv.mx>
2021-04-06 06:23:38 +0000

    conditionals

---
+++ conditionals.py
@@ -0,0 +1,20 @@
+#Luz Medel
+#05/04/21
+#Sistema de Apoyo a la toma de decisiones
+#Secuancia de ADN
+nucleotide = raw_input("Input nucleotide A, C, T, G :")
+print nucleotide
+
+if (nucleotide == "A"):
+ print ("ADENINA")
+elif (nucleotide == "C"):
+ print ("CITOSINA")
+elif (nucleotide == "T"):
+ print ("TIMINA")
+elif (nucleotide == "G"):
+ print("GUANINA")
+else:
+ print("ERROR IN INPUT")
+
+
+
