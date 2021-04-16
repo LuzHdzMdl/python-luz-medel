5eaa4ff1d4b80d32ab467c2b2db19af8b36514f2
LuzHdzMdl <zs18021309@estudiantes.uv.mx>
2021-04-16 16:23:43 +0000

    for-math

---
+++ for-math.py
@@ -0,0 +1,54 @@
+#Luz Medel
+#16 abril 2021
+#for math example
+
+n=10;
+
+#for i in range (1, n+1):
+#  print(i)
+
+#for i in range (1, n+1, 2):
+#  print(i);
+
+#for i in range (2 , n+1, 2):
+#  print(i)
+
+#for i in range (n, (n*10)+1, n):
+#  print(i);
+
+#for i in range (n, 0, -1):
+#  print (i)
+
+#sum
+#avg
+#1.. n
+
+#n=5
+#sum=0
+
+#for i in range (1, n+1):
+#  print (i)
+#  sum = sum + i
+ 
+#avg= sum/n
+#print ("sum 1..", n, sum)
+#print ("Average ", avg)
+
+#fact=1
+#n=3
+
+#for i in range(1, n+1):
+#  print (i)
+#  fact = fact * i
+
+#print "n! = ", fact
+
+
+#f(x) = x^3
+
+for i in range (-10, 10+1, 1):
+  fx = i*i
+  print i," , ", fx
+
+
+
